80e08d71e11631dcd5652ac6bf564c492f1802e0
chaeyk <chaeyk@neowiz.com>
2023-11-27 10:57:43 +0900

    tsx 로 실행

---
+++ README.md
@@ -0,0 +1,9 @@
+# 실행법
+
+다음과 같이 .env 파일을 생성
+
+```
+OPENAI_API_KEY=xxx
+```
+
+```npm start``` 로 실행
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const chatCompletion = await openai.chat.completions.create({
   messages: [
     { role: 'system', content: 'You are a skillful translator who translates English to Korean. You know the context of text, and you can choose prefer word for that context. You try to translate to the most natural Korean possible. The translated result should look like text written by a Korean person, not like a machine translation. Input text is json format, and you translate json values (not key), and returns replaced json. Preserve html tag or like somethings.'},
-    { role: 'user', content: JSON.stringify({ teacher: 'This is a test.', student: 'I\'ll do my homework &gt; at night.'}) },
+    { role: 'user', content: JSON.stringify({ teacher: 'This is a test.', student: 'I\'ll do my &lt; homework &gt; at night.'}) },
   ],
   model: 'gpt-4',
 });
